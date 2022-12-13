3accbbd5542a8f83907664bd77400899acbbcda8
wintersun <nerverstop@163.com>
2022-12-13 17:43:56 +0800

    add app icons

--- config.h
+++ config.h
@@ -9,6 +9,7 @@ const Block blocks[] = {
   BLOCK("sb-nettraf", 1, 16),
   BLOCK("sb-disk", 5, 13),
   BLOCK("sb-memory", 5, 14),
-  BLOCK("sb-volume", 0, 22),
-  BLOCK("sb-clock", 1, 1)
+  BLOCK("sb-volume", 0, 21),
+  BLOCK("sb-clock", 1, 1),
+  BLOCK("sb-app", 1, 9)
 };
