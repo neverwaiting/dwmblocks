60df60fff8da5ae927032a4e0c068c01adf18187
wintersun <nerverstop@163.com>
2022-12-23 19:49:41 +0800

    add sb-gpu script and resort statusbar

--- config.h
+++ config.h
@@ -4,11 +4,12 @@
 
 const Block blocks[] = {
   BLOCK("sb-forecast", 5, 5),
-  BLOCK("sb-cpu", 1, 18),
   BLOCK("sb-pacpackages", 3600, 8),
+  BLOCK("sb-cpu", 1, 18),
+  BLOCK("sb-gpu", 1, 3),
+  BLOCK("sb-memory", 5, 14),
   BLOCK("sb-nettraf", 1, 16),
   BLOCK("sb-disk", 5, 13),
-  BLOCK("sb-memory", 5, 14),
   BLOCK("sb-volume", 0, 21),
   BLOCK("sb-clock", 1, 1),
   BLOCK("sb-app", 1, 9)
